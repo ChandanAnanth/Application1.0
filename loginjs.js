dbdb00ce3e37ab64e8e0b2b36bb1a7c56e6d6272
ChandanAnanth <chandanananth@gmail.com>
2018-11-15 15:42:49 +0530

    First commit

---
+++ loginjs.js
@@ -0,0 +1,15 @@
+function validation() {
+            let usrname = document.login.usrnm.value;
+            let passWord = document.login.psw.value;
+
+            if (usrname == "" || passWord == "") {
+                alert("Field should not be empty!!");
+                return;
+            }
+
+            if (usrname == "admin" && passWord == "admin"){
+                document.getElementById("form").action = "home.html"
+            }
+
+    
+        }
